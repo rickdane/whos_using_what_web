d5ee2f1601b62f2b7d240f281cb23506fcd4c482
devs1010 <r.dane1010@gmail.com>
2012-12-10 09:52:29 -0800

    test mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,27 @@ class ApplicationController < ActionController::Base
   layout "application"
   protect_from_forgery
 
+
+  #for mobile version of app
+  before_filter :prepend_view_path_if_mobile
+
+  private
+
+  #for mobile version of app
+  def prepend_view_path_if_mobile
+    if mobile_request?
+      prepend_view_path Rails.root + 'app' + 'mobile_views'
+    end
+  end
+
+  #for mobile version of app
+  def mobile_request?
+    request.subdomains.first == 'm'
+  end
+
+  helper_method :mobile_request?
+
+
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
   end
