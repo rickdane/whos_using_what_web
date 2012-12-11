d6165fbd30e40bb2e2326af8806bc70d6eb8551e
devs1010 <r.dane1010@gmail.com>
2012-12-10 19:21:10 -0800

    mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,43 +1,49 @@
 class ApplicationController < ActionController::Base
-  layout "application"
+
   protect_from_forgery
 
-
-  #for mobile version of app
+  before_filter :set_mobile_preferences
+  before_filter :redirect_to_mobile_if_applicable
   before_filter :prepend_view_path_if_mobile
 
   private
 
-  #for mobile version of app
+  def set_mobile_preferences
+    if params[:mobile_site]
+      cookies.delete(:prefer_full_site)
+    elsif params[:full_site]
+      cookies.permanent[:prefer_full_site] = 1
+      redirect_to_full_site if mobile_request?
+    end
+  end
+
   def prepend_view_path_if_mobile
     if mobile_request?
       prepend_view_path Rails.root + 'app' + 'mobile_views'
     end
   end
 
-  #for mobile version of app
+  def redirect_to_full_site
+    redirect_to request.referer.gsub(/m\./, '') and return
+  end
+
+  def redirect_to_mobile_if_applicable
+    unless mobile_request? || cookies[:prefer_full_site] || !mobile_browser?
+      redirect_to request.protocol + "m." + request.host_with_port.gsub(/^www\./, '') +
+                      request.request_uri and return
+    end
+  end
+
   def mobile_request?
-    #hack for request.domain in case issue with subdomain
+    #hack for local testing to check reglar domain too
     request.subdomains.first == 'm' || request.domain.first == 'm'
   end
 
   helper_method :mobile_request?
 
-
-  def current_user
-    @current_user ||= User.find_by_id(session[:user_id])
+  def mobile_browser?
+    request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(iPhone|iPod|iPad|Android)/]
   end
 
-  def signed_in?
-    !!current_user
-  end
-
-  helper_method :current_user, :signed_in?
-
-  def current_user=(user)
-    @current_user = user
-    session[:user_id] = user.id
-    session[:logged_in] = true
-  end
-
+  helper_method :mobile_browser?
 end
