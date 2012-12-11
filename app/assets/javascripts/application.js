12ebc9bd854dc382cdecee2b002976348324d965
devs1010 <r.dane1010@gmail.com>
2012-12-11 01:09:58 -0800

    asset refactoring

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,6 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
-//
+//                          cd
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
@@ -10,6 +10,4 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
 //= require_tree .
