6d20abdbb1a4ac065772076f99cd01adba13ce02
devs1010 <r.dane1010@gmail.com>
2012-12-29 23:35:43 -0800

    update

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ group :development do
   gem 'sqlite3'
 end
 
-gem 'whos_using_what', '~> 1.0.7'
+gem 'whos_using_what', '~> 1.0.9'
 gem 'pg'
 gem 'rack'
 gem 'mongo'
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,5 @@
 require 'whos_using_what/api_clients/linkedin_client'
+require 'whos_using_what/api_clients/simply_hired_client'
 require_relative '../nosql/mongo_helper'
 require 'whos_using_what/data_searchers/companies_searcher'
 require 'whos_using_what/data_gatherers/geo_tagger'
@@ -19,6 +20,8 @@ class SearchesController < ApplicationController
     @mongo_client = MongoHelper.get_mongo_connection
     @companies_coll = @mongo_client['companies']
     @coords_coll = @mongo_client['coordinates']
+    @simply_hired_client = SimplyHiredClient.new
+
   end
 
   def authenticate_user!
@@ -79,10 +82,122 @@ class SearchesController < ApplicationController
 
   end
 
+  def clean_company_name_helper input_name
+
+    #remove spaces
+    company_name = input_name.gsub(/\s+/, "")
+    company_name.downcase!
+
+  end
+
+  #determines if result has already appeared so we don't repeate results
+  def is_result_dupe? result
+
+    if !@results_dedupe_map
+      @results_dedupe_map = []
+    end
+
+    if result.size > 100
+      result = result[0..99]
+    end
+
+    result.downcase!
+    result = result.gsub(/\s+/, "")
+
+    if @results_dedupe_map.include? result
+      return true
+    end
+
+    @results_dedupe_map.push result
+
+    false
+
+  end
+
+  def prepare_for_url_helper input
+
+    input = input.gsub(/\s+/, "+")
+
+  end
 
   def search
 
     # TODO this is being called twice when search form is submitted, once as POST and once as GET, figure out why and fix this, as it should only call as POST
+
+    cur_user = @@users_collection.find_one(:session_id => session[:session_id])
+
+    # create the linkedin client that is specific to the user TODO see about keeping this cached within session
+    @linkedin_client = LinkedinClient.new(ENV["linkedin.api_key"], ENV["linkedin.api_secret"], cur_user['credentials_linkedin']['token'], cur_user['credentials_linkedin']['secret'], "http://linkedin.com")
+
+    @person_search = params[:person_search]
+
+    @req_zip = @person_search[:zipcode]
+    @req_prog_language = @person_search[:programming_language]
+
+    @results = Array.new
+
+    xml_resp = @simply_hired_client.perform_search @req_prog_language, "san francisco, ca"
+
+    company_containers = Hash.new
+    company_names = []
+
+    job_listings = xml_resp['shrs']['rs']['r']
+
+    job_listings.each do |job|
+
+      #skip if its a dupe
+      # TODO verify whether this is necessary
+      if is_result_dupe? job['e']
+        next
+      end
+
+      if !job['cn'].is_a? String
+        next
+      end
+
+      company_name_clean = clean_company_name_helper job['cn']
+
+      container = company_containers[company_name_clean]
+
+      job_map = {
+          :title => job['jt'],
+          :location => job['loc'],
+          :description => job['e']
+      }
+
+      if !container
+        company_names.push company_name_clean
+        url_company_name = prepare_for_url_helper job['cn']
+        url_keyword = prepare_for_url_helper @req_prog_language
+
+        container = {
+            :company => {
+                :name => job['cn'],
+                :url => job['src']
+            },
+            :jobs => [],
+            :linkedin_search_urls => {
+                :hiring => "http://www.linkedin.com/search/fpsearch?keywords=" << "human" << "&company=" << url_company_name << "&currentCompany=C",
+                :keyword => "http://www.linkedin.com/search/fpsearch?keywords=" << url_keyword << "&company=" << url_company_name << "&currentCompany=C"
+            }
+        }
+        company_containers[company_name_clean] = container
+
+        @results.push container
+
+      end
+
+      container[:jobs].push job_map
+
+    end
+
+    render 'searches/search_results'
+
+  end
+
+  # deprecated search method, for reference
+  def depr_search
+
 
     @person_search = params[:person_search]
     @results = Array.new
