9a971231469b9f0d3019521fc093424b80b964fc
devs1010 <r.dane1010@gmail.com>
2012-12-30 15:40:19 -0800

    update

--- app/models/person_search.rb
+++ app/models/person_search.rb
@@ -1,4 +1,4 @@
 class PersonSearch < ActiveRecord::Base
-  attr_accessible :programming_language, :zipcode, :exclude_recruiters, :page_number
+  attr_accessible :programming_language, :page_number, :zipcode, :exclude_recruiters
 
 end
