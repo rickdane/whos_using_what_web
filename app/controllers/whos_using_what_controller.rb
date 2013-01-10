4ae198bacbd62061c84d4833f9f2e69964e53954
devs1010 <r.dane1010@gmail.com>
2013-01-10 00:55:24 -0800

    update

--- app/controllers/whos_using_what_controller.rb
+++ app/controllers/whos_using_what_controller.rb
@@ -1,10 +1,11 @@
 require 'whos_using_what/api_clients/linkedin_client'
 require_relative '../nosql/mongo_helper'
+require 'whos_using_what/util/map_data_extraction_util'
 
 class WhosUsingWhatController < ApplicationController
 
   @@base_url = "http://api.linkedin.com/v1/"
-  @@num_pages_to_scrape = 4
+  @@num_pages_to_scrape = 25
   @@location = 'us:82'
 
   def show
@@ -40,12 +41,47 @@ class WhosUsingWhatController < ApplicationController
     results = results['people']['values']
 
     results.each do |result|
-      company = result['positions']['values'][0]['company']
+      if !result
+        next
+      end
+
+      keys_arr1 = ["positions", "values", 0, "company"]
+      company = MapDataExtractionUtil.safe_extract_helper keys_arr1, result, nil, nil
+
+      if !company
+        next
+      end
 
       existing_company = @whos_using_what_coll.find_one('id' => company['id'])
 
       if (!existing_company)
         company['keyword_count'] = 1
+        company['location'] = @@location
+
+        #calculate the range from the string value
+        begin
+          range_str = company['size']
+          if range_str
+            range_arr = range_str.split " ".first
+            range_str = range_arr[0]
+            range_arr = range_str.split "-"
+            if !range_arr[0].include? '+'
+              low_range = Integer(range_arr[0])
+              high_range = Integer(range_arr[1])
+            else
+              range_arr[0]["+"] = ""
+              range_arr[0][","] = ""
+              low_range = Integer(range_arr[0])
+              #this is to give an integer value, rather than 10,000+, for example
+              high_range = low_range + 1
+            end
+
+            company['employee_range_low'] = low_range
+            company['employee_range_high'] = high_range
+          end
+        rescue Exception
+
+        end
         @whos_using_what_coll.insert(company)
       else
         company = existing_company
