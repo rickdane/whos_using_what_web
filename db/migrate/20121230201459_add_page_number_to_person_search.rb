31569515f9f01a3a37620cdd2e5a7608cc366cdd
devs1010 <r.dane1010@gmail.com>
2013-01-13 20:49:01 -0800

    update

--- db/migrate/20121230201459_add_page_number_to_person_search.rb
+++ db/migrate/20121230201459_add_page_number_to_person_search.rb
@@ -1,5 +1,5 @@
 class AddPageNumberToPersonSearch < ActiveRecord::Migration
   def change
-    add_column :person_searches, :page_number, :string
+    #add_column :person_searches, :page_number, :string
   end
 end
