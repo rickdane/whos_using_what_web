977bd8d4cbac4f35e31a04541471c4e50c69c8b2
devs1010 <r.dane1010@gmail.com>
2013-03-03 17:14:44 -0800

    quick update

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -1,9 +1,10 @@
-class CalculatorController < ApplicationController
+class CalculatorsController < ApplicationController
 
   layout 'searches'
 
   def new
 
+    puts "----> calculator"
     render :template => "searches/new"
 
   end
